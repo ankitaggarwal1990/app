6d85dae6bfaaa6d739869958a577a9968c181e8f
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-22 16:58:27 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,7 +1,7 @@
 <?php
 
-//$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
-$con = mysqli_connect('localhost','root','','database');
+$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
+//$con = mysqli_connect('localhost','root','','database');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
