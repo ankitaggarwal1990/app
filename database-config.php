8bf928825dfd67358866544e6e53bc1260ea16a6
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-22 16:55:43 +0530

    c

--- database-config.php
+++ database-config.php
@@ -1,18 +1,45 @@
 <?php
-   // define database related variables
-   $database = 'database';
-   $host = 'localhost';
-   $user = 'root';
-   $pass = '';
+  
 
-   // try to conncet to database
-   $dbh = new PDO("mysql:dbname={$database};host={$host};port={3306}", $user, $pass);
+// PHP Data Objects(PDO) Sample Code:
    
-   //$conn = mysql_connect($host, $user, $pass);
+?>
+<?php
 
-   if(!$dbh){
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-      echo "unable to connect to database";
-   }
-   
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  echo $connectstr_dbhost ;
+  echo $connectstr_dbname ;
+  echo $connectstr_dbusername ; 
+  echo $connectstr_dbpassword ;
+}
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+$dbh = new PDO("mysql:dbname={$connectstr_dbname};host={$connectstr_dbhost};port={3306}", $connectstr_dbusername, $connectstr_dbpassword);
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+echo "hghjfhj";
+//mysqli_close($link);
 ?>
