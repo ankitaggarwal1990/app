c555a2c2853e4281f0b9db7da8bc4538e7e8afdf
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-27 17:38:55 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,6 +1,19 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "password@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "trident@trident1", "pwd" => "password@123", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:trident1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+//$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
 //$con = mysqli_connect('localhost','root','','database');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
