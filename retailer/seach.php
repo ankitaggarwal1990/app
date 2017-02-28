a6e17c3b2393568af16ca1812a39be3aab23cf3d
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-28 10:55:49 +0530

    Update seach.php

--- retailer/seach.php
+++ retailer/seach.php
@@ -91,15 +91,25 @@ function showUser(str) {
 
 <?php 
 
-$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "password@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
-mysqli_select_db($con,"ajax_demo");
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "trident@trident1", "pwd" => "password@123", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:trident1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+//mysqli_select_db($con,"ajax_demo");
 $sql="SELECT * FROM products";
 
-$result = mysqli_query($con,$sql);
+$result = sqlsrv_query($conn,$sql);
 
 ?>
 <div>
@@ -116,7 +126,7 @@ $result = mysqli_query($con,$sql);
 	<form action="func.php" method="POST"> 
 <?php
 $i=0;
-while($row = mysqli_fetch_array($result)) {
+while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
 	
 	?>
 	
